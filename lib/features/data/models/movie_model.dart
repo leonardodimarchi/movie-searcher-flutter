96155f78b8e1926f1fc27dc5f7d6c6323962cf3f
User <leonardodimarchi@outlook.com>
2022-03-10 19:20:04 -0300

    update(data/models): adding movie model

---
+++ lib/features/data/models/movie_model.dart
@@ -0,0 +1,43 @@
+import 'package:movie-searcher-flutter/features/domain/entities/movie_entity.dart';
+
+
+class MovieModel extends MovieEntity {
+  const MovieModel({
+    required int id, 
+    required String title, 
+    required String description,
+    required String releaseDate,
+    required String image,
+    required String backdropImage,
+    required double average,
+  }) : super(
+    id, 
+    title, 
+    description,
+    releaseDate,
+    image,
+    backdropImage,
+    average
+  );
+
+    factory MovieModel.fromJson(Map<String, dynamic> json) => 
+      MovieModel(
+        id: json['id'], 
+        title: json['title'],
+        description: json['description'],
+        releaseDate: json['releaseDate'],
+        image: json['image'],
+        backdropImage: json['backdropImage'],
+        average: json['average'],
+      );
+
+    Map<String, dynamic> toJson() => {
+      'id': id,
+      'title': title,
+      'description': description,
+      'releaseDate': releaseDate,
+      'image': image,
+      'backdropImage': backdropImage,
+      'average': average
+    };
+}
