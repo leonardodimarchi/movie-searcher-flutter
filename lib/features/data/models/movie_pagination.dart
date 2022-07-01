73ca6f4d712f34b6531159b4988f763d55e2031f
Leonardo Dimarchi <leonardodimarchi@outlook.com>
2022-06-30 22:15:55 -0300

    refactor(data/models): starting pagination at 0

--- lib/features/data/models/movie_pagination.dart
+++ lib/features/data/models/movie_pagination.dart
@@ -2,5 +2,5 @@ import 'package:movie_searcher_flutter/features/data/models/common/base_paginati
 import 'package:movie_searcher_flutter/features/domain/entities/movie_entity.dart';
 
 class MoviePagination extends BasePagination<MovieEntity> {
-    MoviePagination({ int page = 1, List<MovieEntity> list = const [] }) : super(page: page, list: list);
+    MoviePagination({ int page = 0, List<MovieEntity> list = const [] }) : super(page: page, list: list);
 }
