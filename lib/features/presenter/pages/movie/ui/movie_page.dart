d5feec345f0eba32c0e42c010f023b5b285acaa3
Leonardo Dimarchi <leonardodimarchi@outlook.com>
2022-07-13 20:06:05 -0300

    feature(presenter/movie): added movie title and average

--- lib/features/presenter/pages/movie/ui/movie_page.dart
+++ lib/features/presenter/pages/movie/ui/movie_page.dart
@@ -58,72 +58,109 @@ class _MoviePageState extends ModularState<MoviePage, MovieStore> {
                 child: GlowingOverscrollIndicator(
                     axisDirection: AxisDirection.down,
                     color: backgroundColor,
-                    child: Stack(
-                      children: [
-                        ColorFiltered(
-                          colorFilter: ColorFilter.mode(
-                              Colors.black.withOpacity(0.2), BlendMode.dstATop),
-                          child: MovieBanner(
-                            backgroundColor,
-                            height: 400,
-                            imageUrl: state.movie.backdropImage,
-                          ),
+                    child: Stack(children: [
+                      ColorFiltered(
+                        colorFilter: ColorFilter.mode(
+                            Colors.black.withOpacity(0.2), BlendMode.dstATop),
+                        child: MovieBanner(
+                          backgroundColor,
+                          height: 400,
+                          imageUrl: state.movie.backdropImage,
                         ),
-                        SingleChildScrollView(
-                            child: Padding(
-                                padding: const EdgeInsets.only(
-                                  top: 150,
-                                  bottom: 50,
-                                  left: 50,
-                                  right: 50,
-                                ),
-                                child: Column(
-                                  children: [
-                                    Container(
-                                        height: 400,
-                                        decoration: BoxDecoration(
-                                          boxShadow: [
-                                            BoxShadow(
-                                              color: Colors.black.withAlpha(100),
-                                              blurRadius: 10.0
-                                            ),
-                                          ]
-                                        ),
-                                        child: Stack(
-                                          alignment: Alignment.center,
-                                          children: [
-                                            Padding(
-                                              padding: const EdgeInsets.only(
-                                                  bottom: 145
-                                                ),
-                                              child: Container(
-                                                color: Colors.grey[900],
-                                                child: const Center(
-                                                  child:
-                                                      CircularProgressIndicator(),
-                                                ),
+                      ),
+                      SingleChildScrollView(
+                          child: Padding(
+                              padding: const EdgeInsets.only(
+                                top: 150,
+                                bottom: 50,
+                                left: 50,
+                                right: 50,
+                              ),
+                              child: Column(
+                                children: [
+                                  Container(
+                                      height: 400,
+                                      decoration: BoxDecoration(boxShadow: [
+                                        BoxShadow(
+                                            color: Colors.black.withAlpha(100),
+                                            blurRadius: 10.0),
+                                      ]),
+                                      child: Stack(
+                                        alignment: Alignment.center,
+                                        children: [
+                                          Padding(
+                                            padding: const EdgeInsets.only(
+                                                bottom: 145),
+                                            child: Container(
+                                              color: Colors.grey[900],
+                                              child: const Center(
+                                                child:
+                                                    CircularProgressIndicator(),
                                               ),
                                             ),
-                                            Image(
-                                              image:
-                                                  NetworkImage(state.movie.image),
-                                              height: double.infinity,
-                                              width: double.infinity,
-                                              fit: BoxFit.cover,
-                                              alignment: Alignment.center,
-                                            ),
-                                          ],
-                                        ))
-                                  ],
-                                ))),
-                                Positioned(
-                          left: 20,
-                          top: 65,
-                          child: BackButton(
+                                          ),
+                                          Image(
+                                            image:
+                                                NetworkImage(state.movie.image),
+                                            height: double.infinity,
+                                            width: double.infinity,
+                                            fit: BoxFit.cover,
+                                            alignment: Alignment.center,
+                                          ),
+                                        ],
+                                      )),
+                                  const SizedBox(height: 15),
+                                  Row(
+                                    children: [
+                                      Flexible(
+                                        child: Text(
+                                          state.movie.title,
+                                          maxLines: 2,
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .titleLarge
+                                              ?.copyWith(
+                                                color: Colors.white,
+                                                overflow: TextOverflow.ellipsis,
+                                              ),
+                                        ),
+                                      ),
+                                      Container(
+                                        height: 40,
+                                        width: 40,
+                                        decoration: BoxDecoration(
+                                            shape: BoxShape.circle,
+                                            color: Colors.grey[800]!,
+                                            border: Border.all(
+                                                width: 2,
+                                                color: Colors.grey[700]!)),
+                                        child: Center(
+                                          child: Text(
+                                            state.movie.average.toString(),
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .titleMedium
+                                                ?.copyWith(
+                                                  color:
+                                                      state.movie.average >= 5
+                                                          ? Colors.green
+                                                          : Colors.red,
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ))),
+                      Positioned(
+                        left: 20,
+                        top: 65,
+                        child: BackButton(
                             color: Colors.white,
-                            onPressed: () => Modular.to.navigate('/')
-                          ),
-                        ),
+                            onPressed: () => Modular.to.navigate('/')),
+                      ),
                     ])));
           }),
     );
