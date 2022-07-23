6d73fd5b34a8519b599da1c16c798d0257f7fcdd
Leonardo Dimarchi <leonardodimarchi@outlook.com>
2022-07-23 17:53:34 -0300

    feature(domain/entities): added cast_entity

---
+++ lib/features/domain/entities/cast_entity.dart
@@ -0,0 +1,44 @@
+import 'package:equatable/equatable.dart';
+
+class CastEntity extends Equatable {
+  final int id;
+  final bool isAdult;
+  final String knownForDepartment;
+  final String originalName;
+  final String name;
+  final int popularity;
+  final String profilePath;
+  final int castId;
+  final String character;
+  final String creditId;
+  final int order;
+
+  const CastEntity({
+    required this.id,
+    required this.isAdult,
+    required this.knownForDepartment,
+    required this.originalName,
+    required this.name,
+    required this.popularity,
+    required this.profilePath,
+    required this.castId,
+    required this.character,
+    required this.creditId,
+    required this.order,
+  });
+
+  @override
+  List<Object?> get props => [
+        id,
+        isAdult,
+        knownForDepartment,
+        originalName,
+        name,
+        popularity,
+        profilePath,
+        castId,
+        character,
+        creditId,
+        order,
+      ];
+}
