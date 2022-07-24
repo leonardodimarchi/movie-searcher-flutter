0ccc6965091612f0fb47dfe8cdcf14153caa71c9
Leonardo Dimarchi <leonardodimarchi@outlook.com>
2022-07-24 14:56:24 -0300

    feature(domain/entities): added crew_entity

---
+++ lib/features/domain/entities/crew_entity.dart
@@ -0,0 +1,41 @@
+import 'package:equatable/equatable.dart';
+
+class CrewEntity extends Equatable {
+  final int id;
+  final bool isAdult;
+  final String knownForDepartment;
+  final String originalName;
+  final String name;
+  final int popularity;
+  final String profilePath;
+  final String job;
+  final String department;
+  final String creditId;
+
+  const CrewEntity({
+    required this.id,
+    required this.isAdult,
+    required this.knownForDepartment,
+    required this.originalName,
+    required this.name,
+    required this.popularity,
+    required this.profilePath,
+    required this.job,
+    required this.department,
+    required this.creditId,
+  });
+
+  @override
+  List<Object?> get props => [
+        id,
+        isAdult,
+        knownForDepartment,
+        originalName,
+        name,
+        popularity,
+        profilePath,
+        job,
+        department,
+        creditId,
+      ];
+}
